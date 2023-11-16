49ecbc9dca4e3cdf090e03152e3a4dd2176aab03
Brian Richie Sr <brianrichiesr@gmail.com>
2023-11-15 22:41:25 -0800

    complete lab

--- server/app.py
+++ server/app.py
@@ -20,6 +20,31 @@ def index():
     body = {'message': 'Welcome to the pet directory!'}
     return make_response(body, 200)
 
+@app.route("/pets/<int:id>")
+def pet_by_id(id):
+    pet = db.session.get(Pet, id)
+    if pet:
+        body = pet.to_dict()
+        status = 200
+    else:
+        body = {"message": f"Pet {id} not found."}
+        status = 404
+
+    return make_response(body, status)
+
+@app.route("/species/<string:species>")
+def pet_by_species(species):
+    pets = Pet.query.filter_by(species = species.title()).all()
+    pet_list = []
+    for pet in pets:
+        pet_list.append(pet.to_dict())
+    body = {
+        "count": len(pet_list),
+        "pets": pet_list
+    }
+
+    return make_response(body, 200)
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
